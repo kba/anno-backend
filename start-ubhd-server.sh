8848f6fb1bd9d0b4c1502c31654b9a364f7bf406
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-04-12 17:57:12 +0200

    Set BASE_PATH (this needs fixin in the long run)

--- start-ubhd-server.sh
+++ start-ubhd-server.sh
@@ -11,9 +11,13 @@ export ANNO_SERVER_JWT_SECRET='@9g;WQ_wZECHKz)O(*j/pmb^%$IzfQ,rbe~=dK3S6}vmvQL;F
 export ANNO_STORE_MIDDLEWARES='@kba/anno-mw-user-static,@kba/anno-mw-acl-static'
 export ANNO_ACL_RULES="$(cat acl.json)"
 export ANNO_MW_USER_DATA="$(cat users.json)"
-export ANNO_BASE_URL='http://serv42.ub.uni-heidelberg.de/kba'
 export ANNO_STORE='@kba/anno-store-file'
 export ANNO_PORT=3000
 export ANNO_STORE_FILE="$PWD/store.nedb"
 
+export ANNO_OPENAPI_HOST="http://serv42.ub.uni-heidelberg.de"
+export ANNO_OPENAPI_BASEPATH="/kba"
+
+export ANNO_BASE_URL='http://serv42.ub.uni-heidelberg.de/kba'
+export ANNO_BASE_PATH='/kba'
 cd anno/anno-server && make watch
