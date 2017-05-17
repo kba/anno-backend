6d60aaee1c1ec949d8007149830963026a09f62c
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-05-17 15:39:52 +0200

    Pre/Post Hooks: The mechanism formerly known as middleware

--- start.sh
+++ start.sh
@@ -7,10 +7,12 @@ YML2JSON_MIN=true yml2json.js acl.yml
 echo "Compiling user YAML"
 YML2JSON_MIN=true yml2json.js users.yml
 
-export ANNO_SERVER_JWT_SECRET='@9g;WQ_wZECHKz)O(*j/pmb^%$IzfQ,rbe~=dK3S6}vmvQL;F;O]i(W<nl.IHwPlJ)<y8fGOel$(aNbZ'
-export ANNO_STORE_MIDDLEWARES='@kba/anno-mw-user-static,@kba/anno-mw-acl-static'
 export ANNO_ACL_RULES="$(cat acl.json)"
 export ANNO_MW_USER_DATA="$(cat users.json)"
+
+export ANNO_SERVER_JWT_SECRET='@9g;WQ_wZECHKz)O(*j/pmb^%$IzfQ,rbe~=dK3S6}vmvQL;F;O]i(W<nl.IHwPlJ)<y8fGOel$(aNbZ'
+export ANNO_STORE_HOOKS_PRE='@kba/anno-mw-user-static,@kba/anno-mw-acl-static'
+export ANNO_STORE_HOOKS_POST='@kba/anno-store-post-creator'
 export ANNO_STORE='@kba/anno-store-mongodb'
 export ANNO_PORT=3000
 export ANNO_STORE_FILE="$PWD/store.nedb"
