c076a532e592fe9c36d532167ff51f3c97e764f9
Konstantin Baierer <konstantin.baierer@gmail.com>
2017-06-14 16:50:51 +0200

    move config to pm2 config file

--- start.sh
+++ start.sh
@@ -4,23 +4,13 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 PATH="./node_modules/.bin:./anno-common/scripts:$PATH"
 
-export ANNO_ACL_FILE="$PWD/acl.yml"
-export ANNO_USER_FILE="$PWD/users.yml"
-
 echo "Compiling collection.yml"
 YML2JSON_MIN=true yml2json.js collections.yml
 export ANNO_COLLECTION_DATA="$(cat collections.json)"
-
-export ANNO_MONGODB_PORT=27017
-
-export ANNO_SERVER_AUTH='shibboleth'
-export ANNO_STORE_HOOKS_PRE='@kba/anno-plugins:PreUserFile,@kba/anno-plugins:PreAclFile'
-export ANNO_STORE_HOOKS_POST='@kba/anno-plugins:CreatorInjectorFile'
-export ANNO_STORE='@kba/anno-store-mongodb'
-export ANNO_PORT=3000
 
 if [ -e "$PWD/profile.sh" ];then
     . "$PWD/profile.sh"
 fi
 
-cd anno-common/anno-server && make watch
+pm2 kill
+pm2 --no-daemon start pm2.prod.yml
