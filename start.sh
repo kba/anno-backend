b251b19563d25d81d528b2c2d5b4e980c991a7c9
Konstantin Baierer <konstantin.baierer@gmail.com>
2017-07-03 11:45:00 +0200

    start.sh: No need to compile YAML anymore

--- start.sh
+++ start.sh
@@ -4,10 +4,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 PATH="./node_modules/.bin:./anno-common/scripts:$PATH"
 
-echo "Compiling collection.yml"
-YML2JSON_MIN=true yml2json.js collections.yml
-export ANNO_COLLECTION_DATA="$(cat collections.json)"
-
 if [ -e "$PWD/profile.sh" ];then
     . "$PWD/profile.sh"
 fi
